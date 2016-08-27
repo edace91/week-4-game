000e6ad5bc7e6857e7adf4a8bbc5728622b139ad
edace91 <eduace91@gmail.com>
2016-08-27 00:09:45 -0500

    Game latest update, ready for stats

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
 
     // make variables global to the runtime of our application
-    var healthPoints, attackPower, counterAttackPower, isCharacterChosen, isAttacked, isDefeated;
+    var healthPoints, attackPower, counterAttackPower, isCharacterChosen, isEnemyChosen, isAttacked, isDefeated, hawkinsIndiana;
    
     // function to initialize our game. 
     function initializeRPG() {
@@ -12,62 +12,56 @@ $(document).ready(function () {
         isCharacterChosen = false;
         isEnemyChosen = false;
         isAttacked= false;
-        isDefeated= false;    
+        isDefeated= false; 
+        hawkinsIndiana=["Eleven","Hopper","Demogorgon","Brenner"];   
     }
+
+    var statsEleven = {
+        healthPoints: 120;
+        attackPower: 7;
+        counterAttackPower:11;
+    }
+    var statsHopper = {
+        healthPoints: 100;
+        attackPower: 6;
+        counterAttackPower:9;
+    }
+    var statsDemogorgon = {
+        healthPoints: 150;
+        attackPower:8;
+        counterAttackPower:10;
+    }
+    var statsBenner = {
+        healthPoints:100;
+        attackPower:5;
+        counterAttackPower:8;
+    }
+
+
 
     // add an on click listener to all elements that have the class 'number'
     $('.characters').on('click', function () {
         //check if user already picked 
         if (isCharacterChosen) return;
 
-        $('#Eleven').on('click', function () {
+        $('.character').on('click', function(){
             if (isCharacterChosen) return;
+            var hero = this.id;
+            var townies=["Eleven","Hopper","Demogorgon","Brenner"]; 
+            for( i = 0; i < townies.length; i++){
 
-             $('#Hopper').appendTo('#Enemy1');
-             $('#characterHopper').attr('src', "assets/images/attackChiefHopper.png");
-
-             $('#Demogorgon').appendTo('#Enemy2');
-             $('#characterDemogorgon').attr('src',"assets/images/attackDemogorgon.png");
-
-             $('#Brenner').appendTo('#Enemy3');
-             $('#characterBrenner').attr('src', "assets/images/attackBrenner.png");
-             
+                if (hero != townies[i]){
+                    
+                    $('#'+ townies[i]).appendTo('.availableEnemies');
+                    $('#character'+ townies[i]).attr('src', "assets/images/attack"+ townies[i]+".png");    
+                }
+            }
             isCharacterChosen = true;
         });
-
-
-        $('#Hopper').on('click', function () {
+        //move characters to Enemy position if Brenner is chosen 
+       /* $('#Brenner').on('click', function () {
             if (isCharacterChosen) return;
 
-             $('#Eleven').appendTo('#Enemy1');
-             $('#characterEleven').attr('src',"assets/images/attackEleven.png");
-
-             $('#Demogorgon').appendTo('#Enemy2');
-             $('#characterDemogorgon').attr('src',"assets/images/attackDemogorgon.png");
-
-             $('#Brenner').appendTo('#Enemy3');
-             $('#characterBrenner').attr('src', "assets/images/attackBrenner.png");
-            isCharacterChosen = true;
-        });
-
-        $('#Demogorgon').on('click', function () {
-            if (isCharacterChosen) return; 
-
-             $('#Eleven').appendTo('#Enemy1');
-             $('#characterEleven').attr('src',"assets/images/attackEleven.png");
-
-             $('#Hopper').appendTo('#Enemy2');
-             $('#characterHopper').attr('src', "assets/images/attackChiefHopper.png");
-
-             $('#Brenner').appendTo('#Enemy3');
-             $('#characterBrenner').attr('src', "assets/images/attackBrenner.png");
-
-            isCharacterChosen = true;
-        });
-
-        $('#Brenner').on('click', function () {
-            if (isCharacterChosen) return;
-            
              $('#Eleven').appendTo('#Enemy1');
              $('#characterEleven').attr('src',"assets/images/attackEleven.png");
 
@@ -78,15 +72,26 @@ $(document).ready(function () {
              $('#characterDemogorgon').attr('src',"assets/images/attackDemogorgon.png");
 
             isCharacterChosen = true;
+        });*/
+    });
+    $('.availableEnemies').on('click',function(){
+        if (isEnemyChosen) return;
+        $('.character').on('click', function(){
+            if (isEnemyChosen) return;
+            $(this).appendTo('#Defender');
+            var nemesis = this.id;
+            $('#character'+ nemesis).attr('src', "assets/images/defender"+ nemesis +".png"); 
+            isEnemyChosen = true;
+
         });
+
     });
 
-
-    // add an on click listener to all elements that have the class 'operator'
+ 
   
 
 
-    // add an on click listener to all elements that have the class 'equal'
+   
     $('. ').on('click', function () {
         
     });
